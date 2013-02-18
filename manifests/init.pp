526613834857dca818886ccb5d9bd092b6da1478
Carl Alexander <carlalexander@gmail.com>
2013-02-18 13:40:30 -0500

    Missing doc

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,22 @@
+# Class: wordpress
+#
+# This module manages WordPress.
+#
+# Parameters:
+#
+# There are no default parameters for this class.
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# The module works with sensible defaults:
+#
+# node default {
+#   include wordpress
+# }
 class wordpress {
   class { 'wordpress::package': }
 }
